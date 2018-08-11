2b9e4065a25c0a0fb2112582a12cc79f8986b66a
Liran Mery <liranm@ironsrc.com>
2018-08-11 15:35:34 +0300

    add github repos list service

---
+++ src/lib/reposService.js
@@ -0,0 +1,18 @@
+const baseUrl = 'https://api.github.com/users';
+
+export const loadRepos = (username) => 
+     fetch(`${baseUrl}/${username}/repos`)
+        .then(res => res.json())
+        .then(repos => {
+            const sortedArray = [ ...repos ].sort((repoA, repoB) => 
+                repoB.stargazers_count - repoA.stargazers_count
+            );
+
+            return sortedArray.slice(0, 9).map(repo => 
+                ({
+                    name: repo.name,
+                    stargazers_count: repo.stargazers_count,
+                    url: repo.url
+                })
+            );
+        });
