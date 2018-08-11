d3a97ac20f7d7c770d43da5d80e29c7f07ce9d85
Liran Mery <liranm@ironsrc.com>
2018-08-11 20:16:52 +0300

    css fine tuning

--- src/components/RepoItem.js
+++ src/components/RepoItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export const RepoItem = (props) => (
-    <li className="repos-list-item">
-        <a href={props.url} target="_blank" rel="noopener noreferrer" className="repos-list-item__link">{props.name}</a>
-        <p className="repos-list-item__stars">{props.stargazers_count}</p>
+    <li className="repo-item">
+        <a href={props.url} target="_blank" rel="noopener noreferrer" className="repo-item__link">{props.name}</a>
+        <p className="repo-item__stars">{props.stargazers_count}</p>
     </li>
 );
--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { RepoItem } from './RepoItem';
 
-export const RepoList = (props) => (
-    <ul className="repos-list">
-        { props.repos.map(repo => <RepoItem key={repo.id} {...repo} />) }
-    </ul>
-);
+export const RepoList = (props) => {
+    if(!props.repos.length) {
+        return (
+            <p className="repo-list__empty">No repos</p>
+        );
+    }
+
+    return (
+        <ul className="repo-list">
+            { props.repos.map(repo => <RepoItem key={repo.id} {...repo} />) }
+        </ul>
+    );
+};
